5977d022a632b92992fc7aa052df006cfa7f775e
jinook929 <jinook929@gmail.com>
2021-01-13 17:23:25 -0500

    commit tree 0465906b98ec5fd187bad529f38d5293098da528

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+def hello_t(arr)    
+    if block_given?
+        i = 0
+        while i < arr.length
+            yield(arr[i])
+            i += 1
+        end
+        return arr
+        # arr.each { |name|
+        #     yield(name)
+        # }
+    else
+        puts "Hey! No block was given!"
+    end
 end
 
 # call your method here!
-
+# names = ["Tim", "Tom", "Jim"]
+# hello_t(names){ |name| puts name }
